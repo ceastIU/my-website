3f0f291f9a801a829abd3dd3ebb0501a234526b9
Chris East <ceast@iu.edu>
2020-09-15 19:00:00 -0400

    Updates

---
+++ precache-manifest.47c2e0886e90b129ccb1d146d756c163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58990a9edf4f4e6a32398b87561307c5",
+    "url": "/my-website/links/index.html"
+  },
+  {
+    "revision": "af8fb252b036080354ed",
+    "url": "/my-website/links/static/css/main.35090e71.chunk.css"
+  },
+  {
+    "revision": "06a4041168a4f01022df",
+    "url": "/my-website/links/static/js/2.d200eba7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/my-website/links/static/js/2.d200eba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8fb252b036080354ed",
+    "url": "/my-website/links/static/js/main.bbf3f2b4.chunk.js"
+  },
+  {
+    "revision": "47b8903f16aabb36a5d1",
+    "url": "/my-website/links/static/js/runtime-main.fd695b6a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-website/precache-manifest.895cfe1b4e8a0e03274c6139dba01823.js"
+  "/my-website/links/precache-manifest.47c2e0886e90b129ccb1d146d756c163.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/links/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
